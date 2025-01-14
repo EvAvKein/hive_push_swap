eed8a3967eda4dd8e9f2c69fa0991bba1dd0a59b
Eve Aviv Keinan <evavkein@gmail.com>
2025-01-14 10:17:48 +0200

    refactor push.c to remove temp variable
    in my free time i should maybe make a comment to visualize it, similar to the swap.c comment

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 19:09:15 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/01/08 14:44:55 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/01/14 10:17:23 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,27 @@
 
 static void	push_operation(t_elem **stack1, t_elem **stack2)
 {
-	t_elem	*moved;
-
-	moved = *stack1;
-	if ((*stack1)->next == *stack1)
+	*stack1 = (*stack1)->next;
+	(*stack1)->prev->next = *stack2;
+	*stack2 = (*stack1)->prev;
+	if ((*stack1)->prev == *stack1)
 		*stack1 = NULL;
 	else
 	{
-		(*stack1)->prev->next = (*stack1)->next;
-		(*stack1)->next->prev = (*stack1)->prev;
-		*stack1 = (*stack1)->next;
+		(*stack1)->prev = (*stack1)->prev->prev;
+		(*stack1)->prev->next = *stack1;
 	}
-	if (!*stack2)
+	if (!(*stack2)->next)
 	{
-		moved->next = moved;
-		moved->prev = moved;
+		(*stack2)->next = *stack2;
+		(*stack2)->prev = *stack2;
 	}
 	else
 	{
-		moved->next = *stack2;
-		moved->prev = (*stack2)->prev;
-		(*stack2)->prev->next = moved;
-		(*stack2)->prev = moved;
+		(*stack2)->prev = (*stack2)->next->prev;
+		(*stack2)->next->prev = *stack2;
+		(*stack2)->prev->next = *stack2;
 	}
-	*stack2 = moved;
 }
 
 void	push(t_elem **stack1, t_elem **stack2, char stack2_name)
